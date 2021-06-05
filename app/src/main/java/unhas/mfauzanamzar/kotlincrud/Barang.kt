5cb4c35ffc9f33bea5ace75868c13fcbf34f0fa1
mfauzanamzar <mfauzanamzar@gmail.com>
2021-06-05 21:58:05 +0800

    CRUD App with SQLLite

---
+++ app/src/main/java/unhas/mfauzanamzar/kotlincrud/Barang.kt
@@ -0,0 +1,16 @@
+package unhas.mfauzanamzar.kotlincrud
+
+class Barang {
+
+    var id: Int? = null
+    var name: String? = null
+    var jenis: String? = null
+    var harga: String? = null
+
+    constructor(id: Int, name: String, jenis: String, harga:String){
+        this.id = id
+        this.name = name
+        this.jenis = jenis
+        this.harga = harga
+    }
+}
